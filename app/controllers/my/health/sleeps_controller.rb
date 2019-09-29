9d9fe5604cf8a65368d92b20c163fad5a4c401a5
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-09-29 10:02:27 +0300

    Add ability to edit Sleeps

--- app/controllers/my/health/sleeps_controller.rb
+++ app/controllers/my/health/sleeps_controller.rb
@@ -21,10 +21,28 @@ module My
         end
       end
 
+      def edit
+        @sleep = resource
+      end
+
+      def update
+        @sleep = resource
+        if @sleep.update(post_params)
+          redirect_to my_health_sleeps_path, notice: 'Record was successfully updated'
+        else
+          render :edit
+        end
+      end
+
       private
 
+      # FIXME: Rename to sleep_params
       def post_params
         params.require(:health_sleep).permit(:woke_up_at_hour, :woke_up_at_minutes, :feeling, :notes)
+      end
+
+      def resource
+        current_account.health_sleep.find(params[:id])
       end
     end
   end
