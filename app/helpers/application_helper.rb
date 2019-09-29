82e7840ad3487c36a9b87451fa0ea1380a83e88d
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-09-29 11:07:47 +0300

    Format dates to relative format
    Closes #29

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,17 @@ module ApplicationHelper
     title.presence || t('views.layout.title')
   end
 
+  # FIXME: It should be deleted after updates all #index views
   def long_date(value)
     l(value, format: :long)
+  end
+
+  def human_readable_date(date)
+    if date > (Time.zone.now - 3.days)
+      "#{distance_of_time_in_words(date, Time.zone.now, include_seconds: true)} ago"
+    else
+      l(date, format: :long)
+    end
   end
 
   def feelings_to_array
