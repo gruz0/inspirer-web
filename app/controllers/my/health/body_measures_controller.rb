b6f4091acbc2195c564760e5f82f5dc573cc3031
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-09-29 22:26:34 +0300

    Add ability to create a new BodyMeasure

--- app/controllers/my/health/body_measures_controller.rb
+++ app/controllers/my/health/body_measures_controller.rb
@@ -7,7 +7,25 @@ module My
         @body_measures = current_account.health_body_measure.order(created_at: :desc)
       end
 
-      def new; end
+      def new
+        @body_measure = current_account.health_body_measure.new
+      end
+
+      def create
+        @body_measure = current_account.health_body_measure.new(body_measure_params)
+
+        if @body_measure.save
+          redirect_to my_health_body_measures_path, notice: 'Record was successfully created'
+        else
+          render :new
+        end
+      end
+
+      private
+
+      def body_measure_params
+        params.require(:health_body_measure).permit(:chest, :waist, :hips, :unit, :feeling, :notes)
+      end
     end
   end
 end
