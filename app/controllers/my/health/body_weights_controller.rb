03fa6a6f96d07a596f7e679168572729198303ec
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-09-29 19:16:25 +0300

    Add ability to create a new BodyWeight

--- app/controllers/my/health/body_weights_controller.rb
+++ app/controllers/my/health/body_weights_controller.rb
@@ -7,7 +7,25 @@ module My
         @body_weights = current_account.health_body_weight.order(created_at: :desc)
       end
 
-      def new; end
+      def new
+        @body_weight = current_account.health_body_weight.new
+      end
+
+      def create
+        @body_weight = current_account.health_body_weight.new(body_weight_params)
+
+        if @body_weight.save
+          redirect_to my_health_body_weights_path, notice: 'Record was successfully created'
+        else
+          render :new
+        end
+      end
+
+      private
+
+      def body_weight_params
+        params.require(:health_body_weight).permit(:weight, :unit, :feeling, :notes)
+      end
     end
   end
 end
