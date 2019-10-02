d6781a44614da66ad3c52c99c58dd103a9702baf
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-10-02 18:42:56 +0300

    Add ability to create a new Activity Walking

--- app/controllers/my/activity/walking_controller.rb
+++ app/controllers/my/activity/walking_controller.rb
@@ -6,6 +6,26 @@ module My
       def index
         @walkings = current_account.activity_walking.order(created_at: :desc)
       end
+
+      def new
+        @walking = current_account.activity_walking.new
+      end
+
+      def create
+        @walking = current_account.activity_walking.new(walking_params)
+
+        if @walking.save
+          redirect_to my_activity_walking_index_path, notice: 'Record was successfully created'
+        else
+          render :new
+        end
+      end
+
+      private
+
+      def walking_params
+        params.require(:activity_walking).permit(:distance_unit, :distance, :steps, :feeling, :notes)
+      end
     end
   end
 end
