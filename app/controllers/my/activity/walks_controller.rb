c509cde103226bd5d23b6ec8d8682191acc298d4
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-10-02 21:56:48 +0300

    Add ability to edit Activity Walk

--- app/controllers/my/activity/walks_controller.rb
+++ app/controllers/my/activity/walks_controller.rb
@@ -21,10 +21,27 @@ module My
         end
       end
 
+      def edit
+        @walk = resource
+      end
+
+      def update
+        @walk = resource
+        if @walk.update(walk_params)
+          redirect_to my_activity_walks_path, notice: 'Record was successfully updated'
+        else
+          render :edit
+        end
+      end
+
       private
 
       def walk_params
         params.require(:activity_walk).permit(:distance_unit, :distance, :steps, :feeling, :notes)
+      end
+
+      def resource
+        current_account.activity_walk.find(params[:id])
       end
     end
   end
