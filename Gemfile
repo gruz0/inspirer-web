5ac13ad11701d591808d2e6889476e1f73330fd0
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-10-07 23:20:26 +0300

    Add factory_bot_rails gem

--- Gemfile
+++ Gemfile
@@ -34,5 +34,6 @@ end
 
 group :test do
   gem 'database_cleaner'
+  gem 'factory_bot_rails'
   gem 'ffaker'
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 
 require 'rspec/rails'
+require 'factory_bot'
 require 'ffaker'
 
 begin
@@ -25,6 +26,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 
   config.before(:suite) do
+    FactoryBot.lint
     DatabaseCleaner.clean_with(:truncation)
   end
 
