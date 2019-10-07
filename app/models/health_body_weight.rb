eb45e3b08e21e2ed077c44805ab4fc846a9c1448
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-10-07 21:18:33 +0300

    Add HealthBodyWeight's uniqueness validation

--- app/models/health_body_weight.rb
+++ app/models/health_body_weight.rb
@@ -3,8 +3,18 @@
 class HealthBodyWeight < ApplicationRecord
   belongs_to :account
 
+  before_validation :set_created_date, on: [:create]
+
   enum unit: WEIGHT_UNITS
   enum feeling: FEELINGS
 
   validates :weight, presence: true, numericality: { greater_than: 0 }
+  validates :created_date, uniqueness: { scope: :account_id, message: 'should happen once per day' }
+
+  private
+
+  # NOTE: It uses for validate uniqueness with database constraints
+  def set_created_date
+    self.created_date = Time.zone.now
+  end
 end
