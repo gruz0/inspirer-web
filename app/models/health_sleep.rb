739a40b5df8ae8b8c96bfcc34e4531f7069738e2
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-10-07 21:09:41 +0300

    Add HealthSleep's uniqueness validation

--- app/models/health_sleep.rb
+++ app/models/health_sleep.rb
@@ -3,8 +3,18 @@
 class HealthSleep < ApplicationRecord
   belongs_to :account
 
+  before_validation :set_created_date, on: [:create]
+
   enum feeling: FEELINGS
 
   validates :woke_up_at_hour, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 24 }
   validates :woke_up_at_minutes, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 60 }
+  validates :created_date, uniqueness: { scope: :account_id, message: 'should happen once per day' }
+
+  private
+
+  # NOTE: It uses for validate uniqueness with database constraints
+  def set_created_date
+    self.created_date = Time.zone.now
+  end
 end
