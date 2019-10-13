1a0b354742d329816748161f3ba824d06b5edfd7
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-10-13 16:27:18 +0300

    Update ActiveMailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,13 +29,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -52,4 +45,17 @@ Rails.application.configure do
 
   config.assets.digest = false
   config.assets.debug = false
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: ENV.fetch('MAILER_HOST') }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV.fetch('MAILER_SERVER'),
+    domain: ENV.fetch('MAILER_DOMAIN'),
+    port: ENV.fetch('MAILER_PORT'),
+    user_name: ENV.fetch('MAILER_USER_NAME'),
+    password: ENV.fetch('MAILER_PASSWORD'),
+    authentication: 'login',
+    enable_starttls_auto: true
+  }
 end
