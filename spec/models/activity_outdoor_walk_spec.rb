684af9dde500472e53e3418c26c95f2a2895a1f6
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-10-17 09:35:29 +0300

    Normalize ActivityOutdoorWalk#distance attribute

--- spec/models/activity_outdoor_walk_spec.rb
+++ spec/models/activity_outdoor_walk_spec.rb
@@ -12,6 +12,15 @@ RSpec.describe ActivityOutdoorWalk, type: :model do
   it { is_expected.to validate_presence_of(:distance) }
   it { is_expected.to validate_numericality_of(:distance).is_greater_than(0) }
 
+  describe '#normalize_distance' do
+    it 'returns rounded by 1' do
+      model.distance = 17.35
+      model.validate
+
+      expect(model.distance).to eq(17.3)
+    end
+  end
+
   # Distance Unit
   it_behaves_like 'it has enum', :distance_unit, DISTANCE_UNITS
 
