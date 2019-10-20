294a20c3f41a1c5512e3bb131a7707d4ecdb4034
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-10-20 18:56:17 +0300

    Add dry-system gem
    Closes #189

---
+++ config/initializers/container.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'dry/system/container'
+
+class Container < Dry::System::Container
+  configure do |config|
+    config.root = (Pathname.pwd + 'app')
+    config.auto_register = %w[services]
+  end
+
+  load_paths!('services')
+end
+
+Import = Container.injector
+
+Container.finalize! if Rails.env.production?
