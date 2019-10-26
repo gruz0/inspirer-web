b902f36f8d436cb32e23fe0bb2d688b024068f41
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-10-26 13:55:54 +0300

    Add ability to create a new LearningVideo

--- app/controllers/my/learning/videos_controller.rb
+++ app/controllers/my/learning/videos_controller.rb
@@ -3,9 +3,31 @@
 module My
   module Learning
     class VideosController < BaseController
-      def index; end
+      def index
+        @videos = current_account.learning_video.order(created_at: :desc)
+      end
 
-      def new; end
+      def new
+        @video = current_account.learning_video.new
+      end
+
+      def create
+        @video = current_account.learning_video.new(video_params)
+
+        if @video.save
+          FetchLinkTitleWorker.perform_async(@video.class, @video.id, @video.url) if @video.title.blank?
+
+          redirect_to my_learning_videos_path, notice: 'Record was successfully created'
+        else
+          render :new
+        end
+      end
+
+      private
+
+      def video_params
+        params.require(:learning_video).permit(:url, :title, :feeling, :notes)
+      end
     end
   end
 end
