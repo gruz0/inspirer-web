037c1bc3cd33d20480d84c0e24b8e0757be0f87c
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-10-29 10:36:50 +0300

    Disable Rails/OutputSafety for field_error_proc

--- config/initializers/remove_rails_field_with_errors_wrapper.rb
+++ config/initializers/remove_rails_field_with_errors_wrapper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 ActionView::Base.field_error_proc = proc do |html_tag, _|
+  # rubocop:disable Rails/OutputSafety
   html_tag.html_safe
+  # rubocop:enable Rails/OutputSafety
 end
