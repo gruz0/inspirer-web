b5834fca767297431a9fc9bd7139cf057e80c86b
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-11-04 19:58:03 +0300

    Add possibility to edit an Activity Yoga Asana

--- app/controllers/my/activity/yoga/asanas_controller.rb
+++ app/controllers/my/activity/yoga/asanas_controller.rb
@@ -22,10 +22,27 @@ module My
           end
         end
 
+        def edit
+          @asana = resource
+        end
+
+        def update
+          @asana = resource
+          if @asana.update(asana_params)
+            redirect_to my_activity_yoga_asanas_path, notice: 'Record was successfully updated'
+          else
+            render :edit
+          end
+        end
+
         private
 
         def asana_params
           params.require(:activity_yoga_asana).permit(:notes, :feeling)
+        end
+
+        def resource
+          current_account.activity_yoga_asana.find(params[:id])
         end
       end
     end
