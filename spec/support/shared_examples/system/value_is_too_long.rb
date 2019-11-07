ff0ab57401b3c286a2b95ce9d11272f066da11a9
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-11-08 02:37:14 +0300

    Extract length validation to shared example
    Closes #282

---
+++ spec/support/shared_examples/system/value_is_too_long.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'value is too long' do |form_name, field, label, length = 100|
+  before do
+    visit path
+
+    fill_in "#{form_name}[#{field}]", with: FFaker::Lorem.paragraph
+    click_button I18n.t('shared.buttons.save')
+  end
+
+  it 'renders error message' do
+    expect(page).to have_text("#{label} is too long (maximum is #{length} characters)")
+  end
+end
