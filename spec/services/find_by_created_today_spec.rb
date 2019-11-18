6014355580b3647d810fff80139ade82f449d6d5
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-11-18 08:37:52 +0300

    Update specs expectations for FindByCreatedToday

--- spec/services/find_by_created_today_spec.rb
+++ spec/services/find_by_created_today_spec.rb
@@ -10,18 +10,20 @@ RSpec.describe FindByCreatedToday do
   let(:klass) { account.health_body_weight }
 
   context 'when there are no records' do
-    it { is_expected.to be_falsey }
+    it { is_expected.to be_nil }
   end
 
   context 'when record for this day for current account already exists' do
-    before { create(:health_body_weight, account: account) }
+    let(:existed_record) { create(:health_body_weight, account: account) }
 
-    it { is_expected.to be_truthy }
+    before { existed_record }
+
+    it { is_expected.to eq(existed_record) }
   end
 
   context 'when record for this day for another account exists' do
     before { create(:health_body_weight) }
 
-    it { is_expected.to be_falsey }
+    it { is_expected.to be_nil }
   end
 end
