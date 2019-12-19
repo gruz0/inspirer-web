0409ec24ef4a3c0aab60c29c53e8dd1be5f74213
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-12-19 08:51:57 +0300

    Add contracts specs

---
+++ spec/services/health/body_weights/contracts/update_contract_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Health::BodyWeights::Contracts::UpdateContract do
+  subject(:contract) { described_class.new.call(input) }
+
+  let(:errors) { contract.errors.to_h }
+  let(:input) do
+    {
+      attributes: {
+        weight: '98,15',
+        unit: 'kg',
+        feeling: 'good'
+      }
+    }
+  end
+
+  it { is_expected.to be_success }
+end
