429a43adfb97d7261a4c2a77b4d22641ec70452e
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-12-19 08:23:28 +0300

    Add contracts specs

---
+++ spec/services/health/sleeps/contracts/create_contract_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Health::Sleeps::Contracts::CreateContract do
+  subject(:contract) { described_class.new.call(input) }
+
+  let(:errors) { contract.errors.to_h }
+  let(:input) do
+    {
+      attributes: {
+        woke_up_at_hour: 9,
+        woke_up_at_minutes: 58,
+        feeling: 'good'
+      }
+    }
+  end
+
+  it { is_expected.to be_success }
+end
