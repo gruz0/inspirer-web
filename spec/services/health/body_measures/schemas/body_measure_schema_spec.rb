099ef1a9c545e453305743b20d7fc6cbe42e9897
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-12-21 18:35:45 +0300

    Update BodyMeasureSchema

--- spec/services/health/body_measures/schemas/body_measure_schema_spec.rb
+++ spec/services/health/body_measures/schemas/body_measure_schema_spec.rb
@@ -12,8 +12,7 @@ RSpec.describe Health::BodyMeasures::Schemas::BodyMeasureSchema do
         waist: waist,
         hips: hips,
         unit: unit,
-        feeling: feeling,
-        notes: notes
+        feeling: feeling
       }
     }
   end
@@ -22,11 +21,12 @@ RSpec.describe Health::BodyMeasures::Schemas::BodyMeasureSchema do
   let(:hips) { 92.8 }
   let(:unit) { 'cm' }
   let(:feeling) { 'good' }
-  let(:notes) { html_ipsum('My Notes') }
 
   it { is_expected.to be_success }
 
-  %i[chest waist hips].each { |key| include_examples 'it allows to use comma as a delimiter', key }
+  %i[chest waist hips].each do |key|
+    include_examples 'it allows to use comma as a delimiter', key
+  end
 
   describe 'unit' do
     include_examples 'it validates enum', :unit, BODY_MEASURE_UNITS
@@ -35,6 +35,4 @@ RSpec.describe Health::BodyMeasures::Schemas::BodyMeasureSchema do
   describe 'feeling' do
     include_examples 'it validates enum', :feeling, FEELINGS
   end
-
-  include_examples 'it sanitizes html', :notes
 end
