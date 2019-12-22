8a67c1187214abde6c0f6ecd538a90f92ef17cdd
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-12-22 09:25:39 +0300

    Add contracts specs

---
+++ spec/services/health/body_measures/contracts/create_contract_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Health::BodyMeasures::Contracts::CreateContract do
+  subject(:contract) { described_class.new.call(input) }
+
+  let(:errors) { contract.errors.to_h }
+  let(:input) do
+    {
+      attributes: {
+        chest: '90,35',
+        hips: '65,15',
+        waist: '89,91',
+        unit: 'cm',
+        feeling: 'good'
+      }
+    }
+  end
+
+  it { is_expected.to be_success }
+end
