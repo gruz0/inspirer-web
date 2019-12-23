36ada7d66f96aa725dc1848a27d0420277f3326e
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-12-23 21:52:32 +0300

    Add Transactions

---
+++ app/services/learning/books/transactions/update_transaction.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Learning
+  module Books
+    module Transactions
+      class UpdateTransaction < BaseTransaction
+        step :validate, with: 'shared.validate'
+        step :check_url_uniqueness, with: 'shared.check_url_uniqueness'
+        step :assign_attributes, with: 'assign_attributes'
+        step :persist, with: 'shared.persist'
+      end
+    end
+  end
+end
