d783d5ce173fd9359b214ef26bd4a709e042f683
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-12-24 23:36:28 +0300

    Remove Types::SanitizedHTML
    Closes #369

--- app/schemas/types.rb
+++ app/schemas/types.rb
@@ -4,8 +4,4 @@ module Types
   include Dry.Types()
 
   Coercible::FloatWithComma = Types::Float.constructor { |f| Float(f.to_s.tr(',', '.')) if f }
-
-  Coercible::SanitizedHTML = Types::String.constructor do |content|
-    Sanitize.new(remove_contents: true).fragment(content.to_s).strip if content
-  end
 end
