28454cbe0c97326aec2c5c6c89946640e97daf5d
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-12-26 15:46:54 +0300

    Add Transactions

---
+++ spec/services/learning/podcasts/transactions/update_transaction_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Learning::Podcasts::Transactions::UpdateTransaction do
+  let(:expected_steps) do
+    {
+      validate: 'shared.validate',
+      check_url_uniqueness: 'shared.check_url_uniqueness',
+      assign_attributes: 'assign_attributes',
+      persist: 'shared.persist'
+    }
+  end
+
+  include_examples 'it validates transaction steps'
+end
