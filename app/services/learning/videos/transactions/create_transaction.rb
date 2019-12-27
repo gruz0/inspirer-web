744bea775e782eb2354641a2afa69458fd469a09
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-12-27 08:43:22 +0300

    Add Transactions

---
+++ app/services/learning/videos/transactions/create_transaction.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Learning
+  module Videos
+    module Transactions
+      class CreateTransaction < BaseTransaction
+        step :validate, with: 'shared.validate'
+        step :build, with: 'shared.build'
+        step :check_url_uniqueness, with: 'shared.check_url_uniqueness'
+        step :assign_attributes, with: 'assign_attributes'
+        step :persist, with: 'shared.persist'
+      end
+    end
+  end
+end
