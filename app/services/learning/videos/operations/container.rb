6c9f7b5ef8d173c4b3a8686d028133a836c962e4
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-12-27 08:31:18 +0300

    Add Operations

---
+++ app/services/learning/videos/operations/container.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Learning
+  module Videos
+    module Operations
+      class Container
+        extend Dry::Container::Mixin
+        import Shared::Container
+
+        register('assign_attributes') { AssignAttributes.new }
+      end
+    end
+  end
+end
