e9633a5990fcdefeb11af757d4b5c0f817db6b92
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-12-28 00:46:33 +0300

    Add Contracts

---
+++ spec/services/activity/custom_workouts/contracts/update_contract_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Activity::CustomWorkouts::Contracts::UpdateContract do
+  let(:input) do
+    {
+      attributes: {
+        title: 'My Workout',
+        feeling: 'good'
+      }
+    }
+  end
+
+  include_examples 'it validates contract'
+end
