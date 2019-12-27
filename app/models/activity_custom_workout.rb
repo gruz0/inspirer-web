fc1cda83765002d41ad859c1b0903186275e102d
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-12-28 01:16:50 +0300

    Cleanup ActivityCustomWorkout model

--- app/models/activity_custom_workout.rb
+++ app/models/activity_custom_workout.rb
@@ -1,16 +1,5 @@
 # frozen_string_literal: true
 
 class ActivityCustomWorkout < ApplicationRecord
-  include HTMLSanitizer
-  include NotesSanitizer
-  include Normalizers
-
   belongs_to :account
-
-  before_validation :normalize_title
-
-  enum feeling: FEELINGS
-
-  validates :title, presence: true, length: { maximum: 100 }
-  validates :feeling, presence: true
 end
--- spec/models/activity_custom_workout_spec.rb
+++ spec/models/activity_custom_workout_spec.rb
@@ -5,18 +5,5 @@ require 'rails_helper'
 RSpec.describe ActivityCustomWorkout, type: :model do
   subject(:model) { build(:activity_custom_workout) }
 
-  # Account
   it { is_expected.to belong_to(:account) }
-
-  # Title
-  it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_length_of(:title).is_at_most(100) }
-
-  include_examples 'it normalizes title'
-
-  # Feeling
-  include_examples 'it has enum', :feeling, FEELINGS
-
-  # Notes
-  include_examples 'it sanitizes notes'
 end
