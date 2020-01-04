cc732249ed3e4d3006460dd3f99096fe12163d20
Alexander Kadyrov <gruz0.mail@gmail.com>
2020-01-04 22:15:45 +0300

    Add possibility to add new Activity > Running

--- app/controllers/my/activity/running_controller.rb
+++ app/controllers/my/activity/running_controller.rb
@@ -3,7 +3,29 @@
 module My
   module Activity
     class RunningController < BaseController
-      def index; end
+      include Import[service: 'activity.running.service']
+
+      def index
+        super
+      end
+
+      def new
+        super
+      end
+
+      def create
+        super { my_activity_running_index_path }
+      end
+
+      private
+
+      def resource_params
+        params.require(:activity_running).permit(:distance_unit, :distance, :feeling, :notes).to_h.symbolize_keys
+      end
+
+      def resource_class
+        current_account.activity_running
+      end
     end
   end
 end
---
+++ spec/system/create_activity_running_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Creating an Activity Running', type: :system do
+  let(:account) { create(:account) }
+
+  it_behaves_like 'unauthorized' do
+    let(:path) { new_my_activity_running_path }
+  end
+
+  context 'with valid inputs' do
+    before do
+      sign_in(account)
+
+      visit new_my_activity_running_path
+
+      select 'km', from: 'activity_running[distance_unit]'
+      fill_in 'activity_running[distance]', with: '10,55'
+      select 'good', from: 'activity_running[feeling]'
+      click_button 'Save'
+    end
+
+    it 'redirects to my_activity_running_index_path' do
+      expect(page).to have_current_path(my_activity_running_index_path)
+    end
+
+    it 'renders confirmation message' do
+      expect(page).to have_text('Record was successfully created')
+    end
+
+    it 'renders table with a new record' do
+      rows = [{ 'Distance' => '10.5', 'Unit' => 'km', 'Feeling' => 'good' }]
+      expect(page).to have_table('activity_running', with_rows: rows)
+    end
+  end
+
+  context 'with invalid inputs' do
+    before do
+      sign_in(account)
+
+      visit new_my_activity_running_path
+
+      click_button 'Save'
+    end
+
+    it 'redirects to my_activity_running_index_path' do
+      expect(page).to have_current_path(my_activity_running_index_path)
+    end
+
+    it 'renders errors count' do
+      expect(page).to have_text('3 errors prohibited this activity running from being saved')
+    end
+
+    %i[distance_unit distance feeling].each do |key|
+      include_examples 'when field is blank', key
+    end
+  end
+end
