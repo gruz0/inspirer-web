1a56051f41f6d6338a95ab2ca69ad2de5833039d
Alexander Kadyrov <gruz0.mail@gmail.com>
2020-01-12 13:10:41 +0700

    Add VideosController#show

---
+++ spec/system/learning/videos/show_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Viewing the Learning Video', type: :system do
+  let(:account) { create(:account) }
+  let(:learning_video) do
+    create(:learning_video,
+           url: 'http://example.com',
+           title: 'Some Text',
+           feeling: 'amazing',
+           account: account)
+  end
+
+  it_behaves_like 'unauthorized' do
+    let(:path) { my_learning_video_path(learning_video) }
+  end
+
+  context 'with authorized access' do
+    before do
+      sign_in(account)
+
+      visit my_learning_video_path(learning_video)
+    end
+
+    it 'has clickable title' do
+      expect(page).to have_link(nil, href: 'http://example.com', text: 'Some Text')
+    end
+  end
+end
