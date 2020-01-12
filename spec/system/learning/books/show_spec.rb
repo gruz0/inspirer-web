536ad7071b065fc537e7bc1c22d3561d6df67b27
Alexander Kadyrov <gruz0.mail@gmail.com>
2020-01-12 13:23:16 +0700

    Add BooksController#show

---
+++ spec/system/learning/books/show_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Viewing the Learning Book', type: :system do
+  let(:account) { create(:account) }
+  let(:learning_book) do
+    create(:learning_book,
+           title: 'Amazing Book',
+           url: 'http://example.com',
+           feeling: 'amazing',
+           account: account)
+  end
+
+  it_behaves_like 'unauthorized' do
+    let(:path) { my_learning_book_path(learning_book) }
+  end
+
+  context 'with authorized access' do
+    before do
+      sign_in(account)
+
+      visit my_learning_book_path(learning_book)
+    end
+
+    it 'has clickable title' do
+      expect(page).to have_link(nil, href: 'http://example.com', text: 'Amazing Book')
+    end
+  end
+end
