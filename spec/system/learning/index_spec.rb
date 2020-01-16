49856eadeea924f0817d0bbe69711304f0b77271
Alexander Kadyrov <gruz0.mail@gmail.com>
2020-01-16 10:25:15 +0700

    Add specs to LearningController#index view

---
+++ spec/system/learning/index_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Viewing the index view of Learning section', type: :system do
+  it_behaves_like 'unauthorized' do
+    let(:path) { my_learning_index_path }
+  end
+
+  context 'with authorized access' do
+    let(:account) { create(:account) }
+
+    before do
+      sign_in(account)
+
+      visit my_learning_index_path
+    end
+
+    it 'has link to Videos' do
+      expect(page).to have_link(nil, href: my_learning_videos_path, text: 'Videos')
+    end
+
+    it 'has link to Podcasts' do
+      expect(page).to have_link(nil, href: my_learning_podcasts_path, text: 'Podcasts')
+    end
+
+    it 'has link to Articles' do
+      expect(page).to have_link(nil, href: my_learning_articles_path, text: 'Articles')
+    end
+
+    it 'has link to Books' do
+      expect(page).to have_link(nil, href: my_learning_books_path, text: 'Books')
+    end
+  end
+end
