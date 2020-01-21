69d2912fa4020500234d1bd520bf14c867800b3a
Alexander Kadyrov <gruz0.mail@gmail.com>
2020-01-21 19:50:16 +0700

    Update Meditations

--- app/services/health/meditations/schemas/meditation_schema.rb
+++ app/services/health/meditations/schemas/meditation_schema.rb
@@ -7,6 +7,7 @@ module Health
         required(:attributes).hash do
           required(:notes).filled(Types::StrippedString)
           required(:feeling).filled(Types::Feeling)
+          optional(:created_at).maybe(Types::Params::DateTime)
         end
       end
     end
