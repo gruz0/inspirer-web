91f9561e59589fe1b71ebe5e2e787a92a7e895de
Alexander Kadyrov <gruz0.mail@gmail.com>
2020-01-21 16:18:08 +0700

    Update MustBeCreatedOncePerDay to use FindForSpecificDay service

--- app/services/shared/operations/must_be_created_once_per_day.rb
+++ app/services/shared/operations/must_be_created_once_per_day.rb
@@ -6,10 +6,14 @@ module Shared
   module Operations
     class MustBeCreatedOncePerDay
       include Dry::Monads
-      include Import[service: 'find_by_created_today']
+      include Import[service: 'find_for_specific_day']
 
       def call(input)
-        if service.call(input[:resource])
+        resource, attributes = input.values_at(:resource, :attributes)
+
+        existed_record = service.call(resource, attributes[:created_at])
+
+        if existed_record && existed_record.id != resource.id
           Failure ['Record for this day already exists']
         else
           Success input
