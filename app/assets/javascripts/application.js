3a211dd2a8c7fe5068007867e9e8c272dfb70f1b
Alexander Kadyrov <gruz0.mail@gmail.com>
2020-01-22 10:10:08 +0700

    Add Flatpickr to assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,14 @@
 //= require jquery3.min
 //= require popper
 //= require bootstrap-sprockets
+//= require flatpickr
 
+document.addEventListener('DOMContentLoaded', function() {
+  flatpickr('.created-at', {
+    altInput: true,
+    altFormat: "F j, Y H:i",
+    dateFormat: "Y-m-d H:i:s",
+    enableTime: true,
+    time_24hr: true,
+  });
+})
